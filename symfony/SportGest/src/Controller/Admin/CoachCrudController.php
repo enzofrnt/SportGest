248ae50bfc67297bf12c5be91aaf44de6acdb1e2
Fournet Enzo <enzofournet31@gmail.com>
2025-03-12 11:45:58 +0100

    ajout login

--- symfony/SportGest/src/Controller/Admin/CoachCrudController.php
+++ symfony/SportGest/src/Controller/Admin/CoachCrudController.php
@@ -7,11 +7,35 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class CoachCrudController extends AbstractCrudController
 {
+    private const SPECIALITES = [
+        'Fitness' => 'Fitness',
+        'Cardio' => 'Cardio',
+        'Musculation' => 'Musculation',
+        'Crossfit' => 'Crossfit',
+        'Yoga' => 'Yoga',
+        'Pilates' => 'Pilates',
+        'Boxe' => 'Boxe',
+        'Natation' => 'Natation'
+    ];
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Coach::class;
@@ -21,19 +45,66 @@ class CoachCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('nom'),
-            TextField::new('prenom'),
-            EmailField::new('email'),
-            TextField::new('password')
-            ->setFormType(RepeatedType::class)
-            ->setFormTypeOptions([
-                'type' => PasswordType::class,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer le mot de passe'],
-                'invalid_message' => 'Les mots de passe doivent correspondre',
-            ])
-            ->setRequired(true)
-            ->onlyOnForms(),
+            TextField::new('nom', 'Nom'),
+            TextField::new('prenom', 'Prénom'),
+            EmailField::new('email', 'Email'),
+            TextField::new('password', 'Mot de passe')
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'first_options' => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer le mot de passe'],
+                    'invalid_message' => 'Les mots de passe doivent correspondre',
+                    'mapped' => false,
+                ])
+                ->setRequired($pageName === Crud::PAGE_NEW)
+                ->onlyOnForms(),
+            ArrayField::new('specialites', 'Spécialités')
+                ->setFormTypeOptions([
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'entry_type' => \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class,
+                    'entry_options' => [
+                        'choices' => self::SPECIALITES,
+                    ],
+                ])
+                ->setRequired(true),
+            NumberField::new('tarifHoraire', 'Tarif horaire (€)')
+                ->setNumDecimals(2)
+                ->setRequired(true),
         ];
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->passwordHasher->hashPassword($event->getData(), $password);
+            $form->getData()->setPassword($hash);
+        };
     }
 }
--- symfony/SportGest/src/Controller/Admin/SportifCrudController.php
+++ symfony/SportGest/src/Controller/Admin/SportifCrudController.php
@@ -7,11 +7,25 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use App\Enum\NiveauSportif;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SportifCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Sportif::class;
@@ -21,21 +35,62 @@ class SportifCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('nom'),
-            TextField::new('prenom'),
-            EmailField::new('email'),
-            TextField::new('password')
-            ->setFormType(RepeatedType::class)
-            ->setFormTypeOptions([
-                'type' => PasswordType::class,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer le mot de passe'],
-                'invalid_message' => 'Les mots de passe doivent correspondre',
-            ])
-            ->setRequired(true)
-            ->onlyOnForms(),
-    ];
-}
+            TextField::new('nom', 'Nom'),
+            TextField::new('prenom', 'Prénom'),
+            EmailField::new('email', 'Email'),
+            TextField::new('password', 'Mot de passe')
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'first_options' => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer le mot de passe'],
+                    'invalid_message' => 'Les mots de passe doivent correspondre',
+                    'mapped' => false,
+                ])
+                ->setRequired($pageName === Crud::PAGE_NEW)
+                ->onlyOnForms(),
+            DateTimeField::new('dateInscription', 'Date d\'inscription')
+                ->setFormTypeOption('input', 'datetime_immutable')
+                ->setFormat('d/M/Y')
+                ->setRequired(true),
+            ChoiceField::new('niveauSportif', 'Niveau')
+                ->setChoices(array_combine(NiveauSportif::values(), NiveauSportif::values()))
+                ->setRequired(true),
+        ];
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->passwordHasher->hashPassword($event->getData(), $password);
+            $form->getData()->setPassword($hash);
+        };
+    }
 
     /*
     public function configureFields(string $pageName): iterable
