020c0da5aece18ac43a0845c8ac70313a1e9bebe
Fournet Enzo <enzofournet31@gmail.com>
2025-03-13 15:52:29 +0100

    reset

---
+++ symfony/SportGest/src/Controller/Dashboard/DashboardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/dashboard', name: 'dashboard')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Html');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
--- symfony/SportGest/src/Controller/Dashboard/ResponsableCrudController.php
+++ symfony/SportGest/src/Controller/Dashboard/ResponsableCrudController.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Controller\Crud;
+namespace App\Controller\Dashboard;
 
-use App\Entity\FicheDePaie;
+use App\Entity\Responsable;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class FicheDePaieCrudController extends AbstractCrudController
+class ResponsableCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return FicheDePaie::class;
+        return Responsable::class;
     }
 
     /*
