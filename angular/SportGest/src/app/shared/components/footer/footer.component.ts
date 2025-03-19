8b3575335daf0c8434efbc14cee7f3693d7056cc
Fournet Enzo <enzofournet31@gmail.com>
2025-03-19 16:38:16 +0100

    fix footer

--- angular/SportGest/src/app/shared/components/footer/footer.component.ts
+++ angular/SportGest/src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,8 +11,18 @@ import { RouterLink } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
   currentYear: number = 0;
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.currentYear = new Date().getFullYear();
+    this.authService.currentUser$.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 }
